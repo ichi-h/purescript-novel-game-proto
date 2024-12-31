54405e370892ffa0aa640526cf0ba61448d9b34a
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-12-31 11:37:45 +0900

    chore: add aliases

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+      '#': '/output',
+    },
+  },
   server: {
     port: 8080,
   },
